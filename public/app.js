804aeb5b8d59472a81dbbcfe3502e06d97425188
Ryan David Forsyth <forsyth.r@gmail.com>
2019-04-13 12:55:12 -0400

    Initial commit

---
+++ public/app.js
@@ -0,0 +1,35 @@
+let body = document.querySelector("body");
+let lost = false;
+let won = false;
+let clicked = [false, false];
+//
+let status = document.getElementById("status");
+//
+let first = document.createElement("button");
+first.innerText = "one";
+first.addEventListener("click", () => {
+  if (won || lost) return;
+  clicked[0] = true;
+  if (clicked[0] && clicked[1]) {
+    won = true;
+    status.innerText = "you won!";
+  }
+});
+body.appendChild(first);
+let second = document.createElement("button");
+second.innerText = "two";
+second.addEventListener("click", () => {
+  if (won || lost) return;
+  clicked[1] = true;
+  if (clicked[0] && clicked[1]) {
+    won = true;
+    status.innerText = "you won!";
+  }
+});
+body.appendChild(second);
+//////////////////////////////////////////////////////
+setTimeout(() => {
+  if (won || lost) return;
+  lost = true;
+  status.innerText = "You lost!";
+}, 1000);
